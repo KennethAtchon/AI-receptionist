b97b0b296b35da9c4a6def79e701364d50cb3807
Kenneth Atchon <kenneth.atchon@hotmail.com>
2025-10-17 23:33:37 -0700

    sdk initialization

---
+++ src/resources/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Resource exports
+ */
+
+export { CallsResource } from './calls.resource';
+export { SMSResource } from './sms.resource';
+export { EmailResource } from './email.resource';
---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Service exports
+ */
+
+export { ConversationService } from './conversation.service';
+export { ToolExecutionService } from './tool-execution.service';
+export { CallService } from './call.service';
